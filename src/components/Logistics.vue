0e1288689ea7bddbbc7f7c99b6e63c85ef82e27b
Sergio Nifosì <sergio.nifosi@gmail.com>
2022-05-01 09:22:15 +0200

    form e logistic

--- src/components/Logistics.vue
+++ src/components/Logistics.vue
@@ -1,47 +1,105 @@
 <template>
     <div class="logistics py-5">
-        <div class="text-center py-3">
-            <span class="my-text fw-bold my-text-green text-uppercase">Prova</span>
+        <div class="my-top-infos text-center py-3" v-for="(element, index) in logisticalInfos" :key="index + `info`">
+            <span class="my-text fw-bold my-text-green text-uppercase">{{ element.topText }}</span>
                 <h1 class="fw-bold my-3">
                     <span class="px-3 py-2 my-bg-lightgreen my-text-green">
-                        Logistical
+                        {{ element.evidenceTitle }}
                     </span> 
-                    Procedure
+                    {{ element.normalTitle }}
                 </h1>
-                <p class="my-text my-text-color">Testo di prova</p>
+                <p class="my-text my-text-color">{{ element.text }}</p>
+        </div>
                 <div class="py-2">
-                    <div class="d-flex justify-content-evenly align-items-start my-line my-bg-lightgreen">
-                        <div class="d-flex flex-column justify-content-center align-items-center">
-                            <p class="m-0 my-number my-bg-lightgreen">1</p>
-                            <h5 class="my-2 fw-bold">Prova</h5>
-                            <p class="my-text my-text-color">Lorem ipsum dolor sit amet, consectetur</p>
+                    <div class="d-flex justify-content-around align-items-center my-line my-bg-lightgreen mb-4">
+                        <span class="m-0 my-number my-bg-lightgreen my-text-green fw-bold" 
+                        v-for="(element, index) in logisticalProcedure" :key="index">
+                            {{ element.index }}
+                        </span>
+                    </div>
+                    <div class="d-flex justify-content-around align-items-center">
+                        <div class="text-center my-info" v-for="(element, index) in logisticalProcedure" :key="index + `logistics`">
+                            <h5 class="fw-bold">{{ element.title }}</h5>
+                            <p class="m-0 my-text">
+                                {{ element.text }}
+                            </p>
                         </div>
 
                     </div>
                 </div>
-        </div>
+        
     </div>
 </template>
 
 <script>
 export default {
-    name: "LogisticalProcedure"
+    name: "LogisticalProcedure",
+    data: function(){
+        return{
+            logisticalProcedure:[
+                {
+                    index: "01",
+                    title: "Collection of information",
+                    text: "Lorem ipsum dolor sit amet consectetur."
+                },
+                {
+                    index: "02",
+                    title: "Services invoice Sending",
+                    text: "Lorem ipsum dolor sit amet consectetur."
+                },
+                {
+                    index: "03",
+                    title: "Withdrawal of Cargo",
+                    text: "Lorem ipsum dolor sit amet consectetur."
+                },
+                {
+                    index: "04",
+                    title: "Transport Customer Order",
+                    text: "Lorem ipsum dolor sit amet consectetur."
+                },
+                {
+                    index: "05",
+                    title: "Successful Delivery",
+                    text: "Lorem ipsum dolor sit amet consectetur."
+                },
+            ],
+            logisticalInfos:[
+                {
+                    topText: "How it works in practice",
+                    evidenceTitle: "Logistical",
+                    normalTitle: "Procedure",
+                    text: "We work in innovative methodologies to ensure that the entire delivery is done from start to finish as planned."
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 div.logistics{
     min-height: 60vh;
-    border: 1px solid black;
     background-color: #eef4ed;
 }
+
+div.my-top-infos{
+    width: 40%;
+    margin: 0 auto;
+}
+
 div.my-line{
     height: 5px;
 }
+
+div.my-info{
+    width: 180px;
+}
+
 .my-number{
     width: 30px;
     height: 30px;
     line-height: 30px;
+    text-align: center;
     border-radius: 50%;
 }
 </style>
--- src/components/SubsciptionForm.vue
+++ src/components/SubsciptionForm.vue
@@ -2,12 +2,45 @@
     <div class="form">
         <div class="central-container">
             <div class="row py-5">
-                <div class="col-8 py-2">
-                    <h1>form</h1>
+                <div class="col-8 py-2" v-for="(info, index) in formText" :key="index + `info` ">
+                    <span class="my-text fw-bold my-text-green text-uppercase">{{ info.topText }}</span>
+                    <h1 class="fw-bold my-3">
+                        {{ info.normalTitle }}
+                        <span class="px-3 py-2 my-bg-lightgreen my-text-green">
+                            {{ info.evidenceTitle }}
+                        </span> 
+                    </h1>
+                    <p class="my-text my-text-color mb-5">{{ info.text }}</p>
+                    <div class="d-flex gap-2 flex-wrap mb-4">
+                            <input type="text" placeholder="Name" class="my-input w-">
+                            <input type="email" placeholder="Email" class="my-input">
+                            <input type="text" placeholder="Phone" class="my-input">
+                            <input type="text" placeholder="Phone" class="my-input">
+                            <textarea class="my-input w-100" name="text" cols="20" rows="5" placeholder="Message"></textarea>
+                    </div>
+                    <a class="text-uppercase text-white my-bg-green px-3 py-2 text-decoration-none rounded-3" :href="info.button.url">
+                        {{ info.button.text }}
+                    </a>
 
                 </div>
-                <div class="col-4">
-                    <h1>form</h1>
+                <div class="col-4" v-for="(element, index) in formInfo" :key="index + `form`">
+                    <h6 class="fw-bold">{{ element.title }}</h6>
+                    <p class="my-text my-text-color">{{ element.topInfo }}</p>
+                    <p class="my-text my-text-color">{{ element.bottomInfo }}</p>
+                    <div class="mb-4">
+                        <a :href="link.url" v-for="(link, index) in element.links" :key="index + `link`"
+                        class="my-3 my-text-green text-decoration-none d-flex">
+                            <span class="me-1 my-icon my-bg-lightgreen rounded-circle">
+                                <font-awesome-icon :icon="link.icon" />
+                            </span>
+                            <span>
+                                {{ link.text }}
+                            </span>
+                        </a>
+                    </div>
+                    <a :href="element.button.url" class="my-text my-text-green text-decoration-none form-button my-border-green">
+                        {{ element.button.text }}
+                    </a>
                 </div>
             </div>
 
@@ -15,14 +48,81 @@
     </div>
 </template>
 
+
+
 <script>
 export default {
-    name: "SubsciptionForm"
+    nname: "SubsciptionForm",
+    data: function(){
+        return{
+            formText:[
+                {
+                    topText: "Send a Message",
+                    normalTitle: "Get in",
+                    evidenceTitle: "Touch",
+                    text: "We will responde to your message as soon as possible",
+                    button:{
+                        url: "#",
+                        text: "Send"
+                    }
+                }
+            ],
+            formInfo:[
+                {
+                    title: "Example Inc.",
+                    topInfo: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                    bottomInfo: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+                    links:[
+                        {
+                            icon: "fa-solid fa-phone",
+                            url: "#",
+                            text: "1 (305) 12345678",
+                        },
+                        {
+                            icon: "fa-solid fa-envelope",
+                            url: "#",
+                            text: "hello@example.com",
+                        },
+                        {
+                            icon: "fa-solid fa-location-dot",
+                            url: "#",
+                            text: "Main Avenue, 987",
+                        },
+                    ],
+                    button:{
+                        url: "#",
+                        text: "View Map"
+                    }
+                }
+            ],
+        }
+    }
 }
 </script>
+
+
 
 <style lang="scss" scoped>
 div.form{
     min-height: 65vh;
+}
+
+.my-input{
+    background-color: gainsboro;
+    padding: .5rem;
+    border-radius: 5px;
+    width: calc((100% / 2) - .4rem);
+}
+.my-icon{
+    width: 30px;
+    height: 30px;
+    text-align: center;
+    line-height: 30px;
+}
+.form-button{
+    text-transform: uppercase;
+    padding: .5rem 1rem;
+    border-radius: .3rem;
+    font-weight: bold;
 }
 </style>
